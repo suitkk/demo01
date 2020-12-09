dfb1c945e028d4b52e1e537588523ee899b30a87
suitkk <1453176544@qq.com>
2020-12-09 17:29:44 +0800

    元组和字典，函数操作

--- FunctionTest.py
+++ FunctionTest.py
@@ -4,11 +4,24 @@
 #@Softwore : PyCharm
 
 #多个返回值的函数
-def divid(a,b):
-    shang=a//b
-    yushu=a%b
-    return shang,yushu
-n1=int(input("请输入被除数："))
-n2=int(input("请输入除数："))
-x,y=divid(n1,n2)
-print("%d÷%d=%d...%d"%(n1,n2,x,y))
+# def divid(a,b):
+#     shang=a//b
+#     yushu=a%b
+#     return shang,yushu
+# n1=int(input("请输入被除数："))
+# n2=int(input("请输入除数："))
+# x,y=divid(n1,n2)
+# print("%d÷%d=%d...%d"%(n1,n2,x,y))
+
+def fun1():
+    print("--------------")
+def fun2(n):
+    for i in range(n):
+        fun1()
+def fun3(a,b,c):
+    return a+b+c
+def fun4(a,b,c):
+    ava=fun3(a,b,c)/3
+    return ava
+fun2(5)
+print(fun4(2,5,92))
